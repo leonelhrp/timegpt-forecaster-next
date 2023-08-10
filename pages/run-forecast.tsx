ef4569bf64d5ae6edfccc66fc8e3d332902b527c
Leonel Rojas <leonelrojasperez@gmail.com>
2023-08-10 00:20:49 -0400

    chore: remove unnecessary use client

--- pages/run-forecast.tsx
+++ pages/run-forecast.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { AnimatePresence, motion } from "framer-motion";
 import { useState, useEffect } from "react";
 import { ExogenousQuestionType } from "@/types/run-forecast";
